2db03d9a69ab8b5dccd868a7d99f17c37968e6f4
“NofiK” <slavikgrechukh@gmail.com>
2023-01-26 15:35:31 +0200

    some changes

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "0a120382c6b6cf04"; }
+/******/ 		__webpack_require__.h = function() { return "a2567118bef78b51"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */
